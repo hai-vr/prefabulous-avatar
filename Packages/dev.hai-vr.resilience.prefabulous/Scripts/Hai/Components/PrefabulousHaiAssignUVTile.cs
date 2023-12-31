fb91b2e017a23d9ca9722dab2ae9f4d7d74c0c3c
Haï~ <hai-vr@users.noreply.github.com>
2023-12-31 11:05:34 +0100

    uv discard lmao

---
+++ Packages/dev.hai-vr.resilience.prefabulous/Scripts/Hai/Components/PrefabulousHaiAssignUVTile.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using VRC.SDKBase;
+
+namespace Prefabulous.Hai.Runtime
+{
+    [AddComponentMenu("Prefabulous Avatar/PA-H Assign UV Tile")]
+    public class PrefabulousHaiAssignUVTile : MonoBehaviour, IEditorOnly
+    {
+        public string[] blendShapes;
+        public bool limitToSpecificMeshes;
+        public SkinnedMeshRenderer[] renderers;
+
+        public BoxCollider[] vertexContainers;
+        
+        public UVChannel uvChannel = UVChannel.UV1;
+        public int u;
+        public int v;
+        
+        public enum UVChannel {
+            UV0, UV1, UV2, UV3
+        }
+    }
+}
