48ef7d878b369e166ff22ea4ecd794b418cbf453
Haï~ <hai-vr@users.noreply.github.com>
2024-03-20 07:24:18 +0100

    Add Accurate Eye Tracking

---
+++ Packages/dev.hai-vr.resilience.prefabulous/Scripts/Hai/Components/PrefabulousHaiAccurateEyeTracking.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using VRC.SDKBase;
+
+namespace Prefabulous.Hai.Runtime
+{
+    [AddComponentMenu("Prefabulous Avatar/PA-H Accurate Eye Tracking")]
+    public class PrefabulousHaiAccurateEyeTracking : MonoBehaviour, IEditorOnly
+    {
+        public Transform leftEyeYaw;
+        public Transform leftEyePitch;
+        public Transform rightEyeYaw;
+        public Transform rightEyePitch;
+    }
+}
