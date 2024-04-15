221db0516285be87b0dc0c428d7b28dbee31e380
Haï~ <hai-vr@users.noreply.github.com>
2024-04-15 02:15:38 +0200

    Add Generate Twist Bones: - Generate twist bones improves the look of the elbow joint by twisting the part of the lower arm that is closer to the elbow. - This component is in an alpha stage due to implementation difficulties. - Due to execution order difficulties, this component is currently not built to be compatible with neither Modular Avatar Merge Armature nor VRCFury Armature Link; although it's not built to be compatible, it shouldn't be in conflict. - By default, it creates twist bones for upper part of the lower arm, so that the elbow joint looks good. - It uses an Aim Constraint, which is an unusual setup but should provide good results on most arm angles.

---
+++ Packages/dev.hai-vr.resilience.prefabulous/Scripts/Hai/Components/PrefabulousGenerateTwistBones.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+#if VRC_SDK_VRCSDK3
+using IPrefabulousEditorOnly = VRC.SDKBase.IEditorOnly;
+#else
+using Prefabulous.Universal.Shared.Runtime;
+#endif
+
+namespace Prefabulous.Universal.Common.Runtime
+{
+    [AddComponentMenu("Prefabulous/PA Generate Twist Bones (Alpha)")]
+    public class PrefabulousGenerateTwistBones : MonoBehaviour, IPrefabulousEditorOnly
+    {
+        public string[] excludeBraceletsAndWristwatchesBlendshapes;
+        
+        public bool leftElbowJointLowerArm = true;
+        public bool rightElbowJointLowerArm = true;
+        
+        public bool useCustom;
+        public Vector3 upperUpSuggestion = Vector3.forward;
+        public Vector3 lowerUpSuggestion = Vector3.forward;
+        public Transform upper;
+        public Transform lower;
+        public Transform tip;
+        public bool isMainArmature;
+        public AnimationCurve weightDistribution = AnimationCurve.Linear(0, 0, 1f, 1f);
+
+        public bool generateInOptimizingPhase; // Priority
+        public bool generateBeforeModularAvatarMergeArmature;
+    }
+}
