7baa48b656556b4dc1e322a1ec75359253bce9c5
Haï~ <hai-vr@users.noreply.github.com>
2024-04-28 12:27:59 +0200

    Fix Prefabulous cannot run on deprecated VRC_SDK_VRCSDK3 define: - VRC_SDK_VRCSDK3 is deprecated, and not available on some builds. - Remove the conditional for now. - At a later version, this will need to be updated to use a version define in the asmdef.

--- Packages/dev.hai-vr.resilience.prefabulous/Scripts/Hai/Components/PrefabulousGenerateTwistBones.cs
+++ Packages/dev.hai-vr.resilience.prefabulous/Scripts/Hai/Components/PrefabulousGenerateTwistBones.cs
@@ -1,9 +1,5 @@
 ﻿using UnityEngine;
-#if VRC_SDK_VRCSDK3
 using IPrefabulousEditorOnly = VRC.SDKBase.IEditorOnly;
-#else
-using Prefabulous.Universal.Shared.Runtime;
-#endif
 
 namespace Prefabulous.Universal.Common.Runtime
 {
